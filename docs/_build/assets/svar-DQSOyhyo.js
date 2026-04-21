f884c68859902d769334766d76e8ab57f41c85e5
iramdan461 <iramdan461@gmail.com>
2026-04-21 19:58:16 +0700

    docs: update artifact

--- docs/_build/assets/svar-DQSOyhyo.js
+++
@@ -1,1 +0,0 @@
-import"./web-Dsh988zy.js";/* empty css              */import{S as a}from"./svar-DRpNChOx.js";import"./preload-helper-CgER5ffx.js";import"./_commonjsHelpers-Cpj98o6Y.js";export{a as default};
